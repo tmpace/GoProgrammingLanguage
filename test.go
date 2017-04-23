5ec5fa157727cfa1bd658557fd584823a617de85
tmpace <tmpace11@gmail.com>
2017-04-22 22:24:34 -0500

    Add netflag

--- test.go
+++ test.go
@@ -1,5 +1,18 @@
+// This file will be used to test different examples
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	noDelay time.Duration = 0
+	timeout               = 5 * time.Minute
+)
+
 func main() {
-	// This file will be used to test different examples
+	fmt.Printf("%T %[1]v\n", noDelay)
+	fmt.Printf("%T %[1]v\n", timeout)
+	fmt.Printf("%T %[1]v\n", time.Minute)
 }
