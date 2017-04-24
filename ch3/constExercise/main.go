da9f14ff3c045a09ec5e9f569d228eedbe7c1c1b
Trevor Pace <tmpace11@gmail.com>
2017-04-23 22:01:01 -0500

    Finish ex 3.13

---
+++ ch3/constExercise/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+const (
+	_  = iota
+	KB = 1 << (10 * iota)
+	MB
+	GB
+	TB
+	PB
+	EB
+	ZB
+	YB
+)
+
+func main() {
+	fmt.Println(KB)
+	fmt.Println(MB)
+	fmt.Println(GB)
+	fmt.Println(TB)
+	fmt.Println(PB)
+	fmt.Println(EB)
+	fmt.Println(ZB)
+	fmt.Println(YB)
+}
