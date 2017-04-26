43a6892933f3bb8580650852419e31ae3d0bd11e
Trevor Pace <tmpace11@gmail.com>
2017-04-25 21:16:37 -0500

    Changes to test

--- test.go
+++ test.go
@@ -1,18 +1,39 @@
 // This file will be used to test different examples
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
-const (
-	noDelay time.Duration = 0
-	timeout               = 5 * time.Minute
-)
+import "fmt"
 
 func main() {
-	fmt.Printf("%T %[1]v\n", noDelay)
-	fmt.Printf("%T %[1]v\n", timeout)
-	fmt.Printf("%T %[1]v\n", time.Minute)
+	// declare an array whose length will be set to
+	// the number of elements it's initialized with
+	months := [...]string{
+		1:  "January",
+		2:  "February",
+		3:  "March",
+		4:  "April",
+		5:  "May",
+		6:  "June",
+		7:  "July",
+		8:  "August",
+		9:  "September",
+		10: "October",
+		11: "November",
+		12: "December"}
+
+	Q2 := months[4:7]     // ["April", "May", "June"]
+	summer := months[6:9] // ["June", "July", "August"]
+
+	fmt.Println(Q2)
+	fmt.Println(summer)
+
+	for _, s := range summer {
+		for _, q := range Q2 {
+			if s == q {
+				fmt.Printf("%s is in both Q2 and summer\n", s)
+			}
+		}
+	}
+
+	endlessSummer := summer[:5] // ["June", "July", "August", "September", "October"]
+	fmt.Println(endlessSummer)
 }
